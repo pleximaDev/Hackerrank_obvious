c8dd726d5332adc11b53092898768588e25be035
pleximaDev <drscrut@gmail.com>
2020-07-22 22:26:20 +0300

    Default commit

---
+++ Linux_Shell/getting_started_with_conditionals.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read inpt; echo -e "YES\nNO\n" | grep -i $inpt
+
+# read inpt
+
+# if [ "$inpt" = "y" ] | [ "$inpt" = "Y" ]; then
+# echo "YES"
+# elif [ "$inpt" = "n" ] | [ "$inpt" = "N" ]; then
+# echo "NO"
+# fi
+
+
