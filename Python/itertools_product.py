6acefc5e86ef7504292e10311f5f4c41f7ea5876
pleximaDev <drscrut@gmail.com>
2020-07-29 23:18:41 +0300

    Default commit

--- Python/itertools_product.py
+++ Python/itertools_product.py
@@ -1,3 +1,4 @@
+
 from itertools import product
 
 # first_list = raw_input().split(" ")
@@ -6,17 +7,21 @@ first_list = list(map(int, raw_input().split(" ")))
 second_list = list(map(int, raw_input().split(" ")))
 
 out = list(product(first_list,second_list))
-print(list(product(first_list, second_list)))
+# print(list(product(first_list, second_list)))
 stry = str(out)
-print len(stry)
-print stry[5]
 
-print stry[:5]
-print stry[5 + 1:]
-for _ in range(len(stry) - 1):
-    print "_ = ", _
-    if stry[_] == '[' or stry[_] == ']':
-        stry = stry[:_] + stry[(_ + 1):]
+
+# print stry[:5]
+# print stry[5 + 1:]
+
+stry = stry.replace('[', '')
+stry = stry.replace(']', '')
+stry = stry.replace('),', ')')
+
+# for _ in range(len(stry) - 1):
+#     print "_ = ", _
+#     if stry[_] == '[' or stry[_] == ']':
+#         stry = stry[:_] + stry[(_ + 1):]
 print stry
 # str = "("
 # for _ in range(len(out)):
