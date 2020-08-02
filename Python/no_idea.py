e77a7d16a1755c12a593cf48e78cfcecb783daec
pleximaDev <drscrut@gmail.com>
2020-08-02 18:44:56 +0300

    Default commit

--- Python/no_idea.py
+++ Python/no_idea.py
@@ -1,12 +1,12 @@
 n, m = raw_input().split()
 array = raw_input().split()
-A = raw_input().split()
-B = raw_input().split()
+A = list(set(raw_input().split()))
+B = list(set(raw_input().split()))
 happiness = 0
 array = list(set(array))
 
-for _ in range(0, int(n)):
-    for __ in range(0, int(m)):
+for _ in range(0, len(array)):
+    for __ in range(0, len(A)):
         if array[_] == A[__]:
             happiness += 1
         elif array[_] == B[__]:
