79dc1ebc44d96965ee39b4bb5c5f6c5870049fd6
pleximaDev <drscrut@gmail.com>
2020-08-03 12:35:26 +0300

    Default commit

---
+++ Python/triangle_quest.py
@@ -0,0 +1,13 @@
+for i in range(1,input()):
+    print((10**(i)//9)*i)
+
+
+# for i in range(1,input()):
+    # out = 0
+    # for _ in range(0, i): out += i * 10**_
+    # print out
+
+# import sys
+# for i in range(1,input()):
+#     for _ in range(0, i): sys.stdout.write(chr(i))
+#     print ""
