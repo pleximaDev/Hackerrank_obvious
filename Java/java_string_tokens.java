b8fa5cd14b8e66ed5b76bfc09b61630bf20e553d
pleximaDev <drscrut@gmail.com>
2020-08-09 23:37:18 +0300

    Default commit

--- Java/java_string_tokens.java
+++ Java/java_string_tokens.java
@@ -5,8 +5,15 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine().replace("[\\W]", " ").trim();
-        String[] arr = s.split("[\\W]");//.trim();
+        // String s = scan.nextLine().replace("[\\W]", " ").trim();
+        // String s = scan.nextLine().replace("[^\\W']+", " ").trim();
+        String s = scan.nextLine().replaceAll("[^A-Za-z0-9]", " ").trim().replaceAll(" +", " ");
+        // System.out.println(s.replaceAll("[^A-Za-z0-9]", " ").trim());
+        // String tes = s.replaceAll("[^A-Za-z0-9]", " ");
+        // System.out.println(tes.trim().replaceAll(" +", " "));
+        // String[] arr = s.split("[\\W]");//.trim();
+        String[] arr = s.split("[^a-zA-Z0-9']");//.trim();
+        
         // arr.replaceAll("\\s+","");
         // String[] arr = s.split("[,\\s\\-:\\?']");
         // System.out.println(arr.getClass());
@@ -24,12 +31,20 @@ public class Solution {
         }
         */
         int counter = 0;
+        boolean word = false;
         for ( String ss : arr)
         {
+            word = false;
             for(int i = 0; i < ss.length(); i++)
             {
-                if( ss.charAt(i) >= 'a' && ss.charAt(i) <= 'z' || ss.charAt(i) >= 'A' && ss.charAt(i) <= 'Z'){ counter++; break; }
+                if( ss.charAt(i) >= 'a' && ss.charAt(i) <= 'z' || ss.charAt(i) >= 'A' && ss.charAt(i) <= 'Z'){}
+                else 
+                {
+                    break;
+                }
+                word = true;
             }
+            if(word) counter++;
         }
         // System.out.println(arr.length);
         System.out.println(counter);
@@ -42,8 +57,8 @@ public class Solution {
                 if( ss.charAt(i) >= 'a' && ss.charAt(i) <= 'z' || ss.charAt(i) >= 'A' && ss.charAt(i) <= 'Z')
                 {
                     hasChar = true;
-                    break;
                 }
+                else {hasChar = false; break;}
             }
             if(hasChar) System.out.println(ss);
         }
