cd191ac380ab9e74671143bf50c7a5dad093a6c3
pleximaDev <drscrut@gmail.com>
2020-08-10 14:06:29 +0300

    Double.parseDouble()

---
+++ Java/java_BigDecimal.java
@@ -0,0 +1,35 @@
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+      	sc.close();
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            for(int j = 0; j < n - 1 - i; j++)
+            {
+                if(Double.parseDouble(s[j]) < Double.parseDouble(s[j + 1]))
+                {
+                    String temp = s[j];
+                    s[j] = s[j + 1];
+                    s[j + 1] = temp;
+                }
+            }
+        }
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+}
