61b16ae1da1a4d1e81c871378496ab5ea761ebc4
pleximaDev <drscrut@gmail.com>
2020-08-11 16:33:20 +0300

    Default commit

---
+++ Java/java_static_initializer_block.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static Scanner scan = new Scanner(System.in);
+    
+    // static int H = scan.nextInt();
+    static boolean flag = true;
+    static int B, H;
+    static
+    {
+        try
+        {
+            B = scan.nextInt();
+            H = scan.nextInt();
+            if(B <= 0 || H <= 0)
+            {
+                flag = false;
+                throw new Exception("Breadth and height must be positive");
+            }
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+    }
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
+
