240d6d74fd5a5324dc2ec75c7f3d2bd2eef04212
pleximaDev <drscrut@gmail.com>
2020-08-13 23:12:41 +0300

    Default commit

--- Python/calendar_module.py
+++ Python/calendar_module.py
@@ -3,10 +3,10 @@ import datetime
 
 # month, day, year = raw_input().split()
 month, day, year = (int(x) for x in raw_input().split())
-# print(list(calendar.day_name)[calendar.weekday(y, m, d)].upper())
+# print(list(calendar.day_name)[calendar.weekday(year, month, day)].upper())
 
 # datetime(year, day, month).weekday()
 # datetime.datetime.today().weekday()
 
-# ans = datetime.date(year, month, day)
-# print (ans.strftime("%A"))
+ans = datetime.date(year, month, day)
+print ans.strftime("%A").upper()
