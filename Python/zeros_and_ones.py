fd98fb3caa24deedff672f4a263e31f417a99304
pleximaDev <drscrut@gmail.com>
2020-08-16 22:58:21 +0300

    Default commit

--- Python/zeros_and_ones.py
+++ Python/zeros_and_ones.py
@@ -1,16 +1,13 @@
 import numpy
-
+shape = list(map(int, raw_input().split()))
+print (numpy.zeros(shape, dtype = numpy.int))
+print (numpy.ones(shape, dtype = numpy.int))
 # i, j, k = map(int, raw_input().split())
 # for __ in range(k):
 #     print numpy.zeros((i,j), dtype = numpy.int)
 #     if _ is not k - 1:
 #         print
-
 # for _ in range(k):
 #     print numpy.ones((i,j), dtype = numpy.int)
 #     if _ is not k - 1:
 #         print
-
-shape = list(map(int, raw_input().split()))
-print (numpy.zeros(shape, dtype = numpy.int))
-print (numpy.ones(shape, dtype = numpy.int))
